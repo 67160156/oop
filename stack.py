1860bf96dc6b41b2c7a2596b8fba982b7539a8ff
67160156 <67160156@go.buu.ac.th>
2025-11-03 20:21:12 +0700

    Update stack.py

--- stack.py
+++ stack.py
@@ -9,21 +9,17 @@ class Stack:
         if self.is_empty():
             return "Stack is empty"
         return self.list.pop()
-
     def peek(self):
         if not self.is_empty():
             return self.list[-1]
-        return "stack is empty"
-
+        return "Stack is empty"
     def is_empty(self):
         if len(self.list) == 0:
             return True
         else:
             return False
-        
     def size(self):
         return len(self.list)
-
 
 s = Stack()
 s.push(1)
@@ -31,6 +27,7 @@ s.push(2)
 s.push(3)
 s.push(4)
 s.push(5)
+print("Is empty?",s.is_empty())
 
 print("Size after push:",s.size())
 print("Top element:", s.peek())
